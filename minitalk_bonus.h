a68917962d6e25f9051b811ff09bf214bdbaca5a
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2024-07-06 07:53:16 +0100

    bonus add

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmatondo <nmatondo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 12:24:33 by nmatondo          #+#    #+#             */
-/*   Updated: 2024/07/06 07:48:11 by nmatondo         ###   ########.fr       */
+/*   Updated: 2024/07/06 07:48:21 by nmatondo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "./Libft/libft.h"
 
+void	sigusr_handler(int sig, siginfo_t *info, void *context);
 void	send_sms(pid_t pid, char *sms);
-void	sigusr_handler(int sig);
+void	receive(int sig);
 
 #endif
